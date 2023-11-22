76eb1f94724a97a0f722748828c4ca4b484f4f7e
DmitriyMar <dmitriy-maraev@yandex.ru>
2023-11-22 15:29:19 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,22 +1,25 @@
 package ru.job4j.tracker;
 
+/**
+* JavaDoc.
+*/
 public class Item {
-    private int id;
-    private String name;
+  private int id;
+  private String name;
 
-    public int getId() {
-        return id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 }
