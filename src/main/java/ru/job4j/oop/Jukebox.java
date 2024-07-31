f45cc464cdb7420d1abcb65b6bc4448b4f5b39bd
user <dmitriy-maraev@yandex.ru>
2024-07-31 10:23:30 +0300

    1.3. Вызов метода с аргументами. [#166086]

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+    public void music(int position) {
+        String song = switch (position) {
+            case 1 -> "Пусть бегут неуклюже";
+            case 2 -> "Спокойной ночи";
+            default -> "Песня не найдена";
+        };
+        System.out.println("I can sing a song :" + song);
+    }
+
+    public static void main(String[] args) {
+        Jukebox petya = new Jukebox();
+        petya.music(1);
+        petya.music(2);
+        petya.music(3);
+    }
+}
